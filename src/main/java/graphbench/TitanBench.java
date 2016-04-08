19a8e1d32b08513704797da19dcc8d82416203a9
Jong Wook Kim <jongwook@nyu.edu>
2016-04-08 04:31:39 -0400

    now adding edges; adding vertices seems to be linear

--- src/main/java/graphbench/TitanBench.java
+++ src/main/java/graphbench/TitanBench.java
@@ -1,6 +1,7 @@
 package graphbench;
 
 import com.google.common.collect.ImmutableMap;
+import com.thinkaurelius.titan.core.TitanVertex;
 import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
 import org.apache.commons.io.FileUtils;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
@@ -16,15 +17,16 @@ public class TitanBench {
         db.mkdirs();
 
         try (StandardTitanGraph graph = (StandardTitanGraph) GraphFactory.open(ImmutableMap.of(
-                "gremlin.graph", "com.thinkaurelius.titan.core.TitanFactory",
-                "storage.backend", "berkeleyje",
-                "storage.hostname", "127.0.0.1",
-                "storage.directory", "db"
+            "gremlin.graph", "com.thinkaurelius.titan.core.TitanFactory",
+            "storage.backend", "berkeleyje",
+            "storage.directory", "db"
         ))) {
             long started = currentTimeMillis();
 
             for (int i = 0; i < 100000; i++) {
-                graph.addVertex("vertex-" + i);
+                TitanVertex v1 = graph.addVertex("left-" + i);
+                TitanVertex v2 = graph.addVertex("right-" + i);
+                v1.addEdge("e", v2);
                 if (i % 100 == 0) {
                     System.out.printf("Added %d, %.3f seconds\n", i, (currentTimeMillis() - started) / 1000.0);
                 }
